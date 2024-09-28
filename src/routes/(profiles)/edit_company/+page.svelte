7d03bf9060f4d1751b9252c3e5ddcf37faf4b0fc
Eric Biggs <eric@vett.dev>
2024-09-28 06:33:55 -0400

    Adding modal to edit_company

--- src/routes/(profiles)/edit_company/+page.svelte
+++ src/routes/(profiles)/edit_company/+page.svelte
@@ -1090,89 +1090,86 @@
 			>
 		</div>
 	</form>
-</div>
-
-<!-- Main modal -->
-<div class="invisible relative"
-	class:invisible={!invisible}
->
-	<div class="inset-0 flex">
-		<div
-			id="crud-modal"
-			tabindex="-1"
-			aria-hidden="true"
-			
-			class="overflow-y-auto overflow-x-hidden m-auto z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)]"
-		>
-			<div class="relative p-4 w-full max-w-md max-h-full">
-				<!-- Modal content -->
-				<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-					<!-- Modal header -->
-					<div
-						class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
-					>
-						<h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-							Add New Administrator
-						</h3>
-						<button
-							type="button"
-							class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-							data-modal-toggle="crud-modal"
-							on:click={() => (invisible = !invisible)}
+	<!-- Main modal -->
+	<div class="fixed z-50" class:invisible={!invisible}>
+		<div class="grid h-screen place-items-center">
+			<div
+				id="crud-modal"
+				tabindex="-1"
+				aria-hidden="true"
+				class="fixed grid h-screen place-items-center overflow-y-auto overflow-x-hidden m-auto md:inset-0 w-screen bg-slate-950/50"
+			>
+				<div class="relative p-4 w-full max-w-md max-h-full">
+					<!-- Modal content -->
+					<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+						<!-- Modal header -->
+						<div
+							class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
 						>
-							<svg
-								class="w-3 h-3"
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 14 14"
+							<h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+								Add New Administrator
+							</h3>
+							<button
+								type="button"
+								class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+								data-modal-toggle="crud-modal"
+								on:click={() => (invisible = !invisible)}
 							>
-								<path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-								/>
-							</svg>
-							<span class="sr-only">Close modal</span>
-						</button>
-					</div>
-					<!-- Modal body -->
-					<form class="p-4 md:p-5 flex flex-col">
-						<div class="gap-4 mb-4">
-							<div class="">
-								<label
-									for="name"
-									class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label
+								<svg
+									class="w-3 h-3"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 14 14"
 								>
-								<input
-									type="text"
-									name="modalemail"
-									id="modalemail"
-									class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-									placeholder="Type admin email"
-								/>
-							</div>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+									/>
+								</svg>
+								<span class="sr-only">Close modal</span>
+							</button>
 						</div>
-						<button
-							type="submit"
-							class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-end"
-						>
-							<svg
-								class="me-1 -ms-1 w-5 h-5"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-								><path
-									fill-rule="evenodd"
-									d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-									clip-rule="evenodd"
-								></path></svg
+						<!-- Modal body -->
+						<form class="p-4 md:p-5 flex flex-col">
+							<div class="gap-4 mb-4">
+								<div class="">
+									<label
+										for="name"
+										class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+										>Email</label
+									>
+									<input
+										type="text"
+										name="modalemail"
+										id="modalemail"
+										class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+										placeholder="Type admin email"
+									/>
+								</div>
+							</div>
+							<button
+								type="submit"
+								class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 self-end"
 							>
-							Send Invite
-						</button>
-					</form>
+								<svg
+									class="me-1 -ms-1 w-5 h-5"
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										fill-rule="evenodd"
+										d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+										clip-rule="evenodd"
+									></path></svg
+								>
+								Send Invite
+							</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
