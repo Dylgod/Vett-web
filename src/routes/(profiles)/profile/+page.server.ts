41ec975de45d9649082df77d031b1d5c9d8d0f6e
Dylgod <dylan@shyapply.com>
2024-09-30 18:44:48 -0400

    This is the last time im using this stack

--- src/routes/(profiles)/profile/+page.server.ts
+++ src/routes/(profiles)/profile/+page.server.ts
@@ -1,15 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
 export async function load({ locals }) {
     const user_session = await locals.supabase.auth.getSession();
   
-    // const { data, error } = await supabaseHook.from('users')
-    //   .eq('id', user_session?.user?.id)
-    //   .single();
-  
-    // if (error) {
-    //   return fail(404, { message: 'Cannot find customer information' });
-    // }
-  
-    return {
-        email: user_session.data.session?.user.email
+    if (!user_session.data.session){
+        redirect(303 ,"/login")
+    } else {
+        return {
+            email: user_session.data.session?.user.email
+        }
     }
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
-<script>
+<script lang="ts">
 	import "../app.css"
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 
 	export let data;
+	let { supabase, session } = data
 	$: ({ session, supabase } = data);
+	
 
 	onMount(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
