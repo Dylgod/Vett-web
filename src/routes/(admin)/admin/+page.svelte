9f92dd5f9f867a38b38d378a7455cd57c4ccc568
Dylgod <dylan@shyapply.com>
2024-10-02 19:53:14 -0400

    testing admin

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -45,83 +45,95 @@
 					<div
 						class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(2rem+1px)]"
 					>
-						<div class="p-5 flex flex-col h-full">
+						<div class="flex flex-col h-full">
 							<div class="overflow-y-auto flex-grow pr-2">
-								<ul role="list" class="divide-y divide-gray-100">
-									<li class="flex justify-between py-5 bg-yellow-700 rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(1rem+1px)]">
-										<div class="flex min-w-0 gap-x-4 flex-grow overflow-hidden">
-											<img
-												class="ml-3 h-12 w-12 flex-shrink-0 rounded-full bg-gray-50"
-												src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-												alt=""
-											/>
-											<div class="min-w-0 flex-auto">
-												<p class="text-sm font-semibold leading-6 text-gray-900 truncate">
-													<a href="#" class="hover:underline">Leslie Alexander</a>
-												</p>
-												<p class="mt-1 text-xs leading-5 text-gray-500 truncate">
-													<a href="mailto:leslie.alexander@example.com" class="hover:underline"
-														>leslie.alexander@example.com</a
-													>
-												</p>
-											</div>
-										</div>
-										<div class="flex shrink-0 items-center gap-x-4 ml-4">
-											<div class="hidden sm:flex sm:flex-col sm:items-end">
-												<p class="text-sm leading-6 text-gray-900 whitespace-nowrap">
-													Co-Founder / CEO
-												</p>
-												<p class="mt-1 text-xs leading-5 text-gray-500 whitespace-nowrap">
-													Last seen <time datetime="2023-01-23T13:23Z">3h ago</time>
-												</p>
-											</div>
-											<div class="relative flex-none">
-												<button
-													type="button"
-													class="block p-2 text-gray-500 hover:text-gray-900"
-													id="options-menu-0-button"
-													aria-expanded="false"
-													aria-haspopup="true"
-												>
-													<span class="sr-only">Open options</span>
-													<svg
-														class="h-5 w-5"
-														viewBox="0 0 20 20"
-														fill="currentColor"
-														aria-hidden="true"
-													>
-														<path
-															d="M10 3a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM10 8.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM11.5 15.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z"
-														/>
-													</svg>
-												</button>
-
-												<div
-													class="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-													role="menu"
-													aria-orientation="vertical"
-													aria-labelledby="options-menu-0-button"
-													tabindex="-1"
-												>
-													<a
-														href="#"
-														class="block px-3 py-1 text-sm leading-6 text-gray-900"
-														role="menuitem"
-														tabindex="-1"
-														id="options-menu-0-item-0"
-														>Demote User<span class="sr-only">, Leslie Alexander</span></a
-													>
-													<a
-														href="#"
-														class="block px-3 py-1 text-sm leading-6 text-gray-900"
-														role="menuitem"
-														tabindex="-1"
-														id="options-menu-0-item-1"
-														>Remote<span class="sr-only">, Leslie Alexander</span></a
-													>
+								<ul role="list" class="divide-y divide-gray-100 pl-3 pr-3">
+									<li
+										class=" w-full bg-cyan-700 rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(1rem+1px)] mt-2"
+									>
+										<button
+											on:click={() => alert('clicked')}
+											class=" min-w-0 overflow-hidden w-full"
+										>
+											<div class="flex gap-3 py-2 px-4 w-full justify-between">
+												<div class="flex gap-3 w-full">
+												<img
+													class="h-16 w-16 flex-shrink-0 rounded-full bg-gray-50"
+													alt=""
+													src="screenshots/eric.jpg"
+												/>
+												<div class="min-w-0 flex flex-col place-items-start col-span-2">
+													<p class="font-semibold text-yellow-400 truncate text-lg">TASK UPDATED</p>
+													<p class="font-semibold text-gray-300 truncate text-sm">Shy Apply LLC</p>
+													<p class="font-semibold text-gray-300 truncate text-sm">
+														PB: Dylan Taylor
+													</p>
 												</div>
 											</div>
-										</div>
+												<div class="pr-2">
+													<p class="text-sm font-semibold text-gray-300 truncate">#113456</p>
+													<p class="text-sm font-semibold text-gray-300 truncate mt-7">10/30/24</p>
+												</div>
+											</div>
+										</button>
+									</li>
+									<li
+										class=" w-full bg-orange-700 rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(1rem+1px)] mt-2"
+									>
+										<button
+											on:click={() => alert('clicked')}
+											class=" min-w-0 overflow-hidden w-full"
+										>
+											<div class="flex gap-3 py-2 px-4 w-full justify-between">
+												<div class="flex gap-3 w-full">
+												<img
+													class="h-16 w-16 flex-shrink-0 rounded-full bg-gray-50"
+													alt=""
+													src="screenshots/eric.jpg"
+												/>
+												<div class="min-w-0 flex flex-col place-items-start col-span-2">
+													<p class="font-semibold text-yellow-400 truncate text-lg">CREATE TAKEHOME</p>
+													<p class="font-semibold text-gray-300 truncate text-sm">Shy Apply LLC</p>
+													<p class="font-semibold text-gray-300 truncate text-sm">
+														PB: Dylan Taylor
+													</p>
+												</div>
+											</div>
+												<div class="pr-2">
+													<p class="text-sm font-semibold text-gray-300 truncate">#113456</p>
+													<p class="text-sm font-semibold text-gray-300 truncate mt-7">10/30/24</p>
+												</div>
+											</div>
+										</button>
+									</li>
+									<li
+										class=" w-full bg-lime-800 rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(1rem+1px)] mt-2"
+									>
+										<button
+											on:click={() => alert('clicked')}
+											class=" min-w-0 overflow-hidden w-full"
+										>
+											<div class="flex gap-3 py-2 px-4 w-full justify-between">
+												<div class="flex gap-3 w-full">
+												<img
+													class="h-16 w-16 flex-shrink-0 rounded-full bg-gray-50"
+													alt=""
+													src="screenshots/eric.jpg"
+												/>
+												<div class="min-w-0 flex flex-col place-items-start col-span-2">
+													<p class="font-semibold text-yellow-500 truncate text-lg">ONBOARDING</p>
+													<p class="font-semibold text-gray-300 truncate text-sm">Shy Apply LLC</p>
+													<p class="font-semibold text-gray-300 truncate text-sm">
+														PB: Dylan Taylor
+													</p>
+												</div>
+											</div>
+												<div class="pr-2">
+													<p class="text-sm font-semibold text-gray-300 truncate">#113456</p>
+													<p class="text-sm font-semibold text-gray-300 truncate mt-7">10/30/24</p>
+												</div>
+											</div>
+										</button>
 									</li>
 								</ul>
 							</div>
