797688b0063722343bf2cf35a5a10ec40bf6be4a
Dylgod <dylan@shyapply.com>
2024-10-11 18:31:23 -0400

    made names required and updated model. need to revisit for logo l8r

--- src/routes/(user)/profile/+page.svelte
+++ src/routes/(user)/profile/+page.svelte
@@ -367,32 +367,6 @@
 							<h3 class="text-lg font-semibold text-gray-900 dark:text-white">
 								Complete your profile
 							</h3>
-							<button
-								type="button"
-								class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-								data-modal-toggle="crud-modal"
-								on:click={() => {
-									edit_profile_invisible = !edit_profile_invisible;
-									editProfile = !editProfile;
-								}}
-							>
-								<svg
-									class="w-3 h-3"
-									aria-hidden="true"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 14 14"
-								>
-									<path
-										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-									/>
-								</svg>
-								<span class="sr-only">Close modal</span>
-							</button>
 						</div>
 						<form method="POST" action="?/updateUser" use:enhance on:submit={resetProfileModal}>
 							<div class="space-y-12">
@@ -439,6 +413,8 @@
 													type="text"
 													name="new_profile_name"
 													id="new_profile_name"
+													required
+													autocomplete='off'
 													bind:value={new_profile_name}
 													class="border border-gray-300 text-gray-900 dark:text-white bg-gray-600 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-primary-500 dark:focus:border-primary-500"
 													placeholder="Ex: Jon Doe"
